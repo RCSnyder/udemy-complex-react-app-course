aaddd7f47b65aab4e5069c9c323774511a0b1877
RCSnyder <44173311+RCSnyder@users.noreply.github.com>
2023-01-29 16:20:39 -0600

    add flash messages login/out

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -17,9 +17,15 @@ function HeaderLoggedOut(props) {
       })
       if (response.data) {
         appDispatch({ type: "login", data: response.data })
+        appDispatch({
+          type: "flashMessage",
+          value: "You have successfully logged in."
+        })
       } else {
-        console.log("Incorrect username/ password")
-        console.log(response.data)
+        appDispatch({
+          type: "flashMessage",
+          value: "Invalid username / password"
+        })
       }
     } catch (e) {
       console.log("There was a problem.")
